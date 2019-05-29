07ad373a2f4899a82f674c1c14c7a9dbaa72043e
Jean-Baptiste Noblot <noblot.jb@gmail.com>
2019-05-29 10:59:43 +0200

    Add 3 pages restaurant - menuList - menuPage

--- src/Controller/RoutingController.php
+++ src/Controller/RoutingController.php
@@ -16,14 +16,14 @@ use Twig\Environment;
 class RoutingController extends AbstractController
 {
     /**
-     * @Route("/menuList", name="menu_list")
+     * @Route("{restaurantName}", name="restaurantDesc")
      * @param Environment $twig
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function menuList(Environment $twig) {
+    public function restaurantPage(Environment $twig) {
 
         $restaurant = new Restaurant();
         $restaurant->name = 'Le Bosphore';
@@ -60,6 +60,91 @@ class RoutingController extends AbstractController
 
 
         $restaurant->hasMenu = $menu;
+        $menu->hasMenuSection[] = $menuSection;
+        $menu->hasMenuSection[] = $menuSection2;
+        $menu->hasMenuSection[] = new MenuSection();
+
+        $content = $twig->render('restaurant.html.twig',
+            [
+                'restaurant' => $restaurant
+            ]);
+
+        return new Response($content);
+    }
+
+    /**
+     * @Route("{restaurantName}/menus", name="menu_list")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function menuList(Environment $twig) {
+
+        $restaurant = new Restaurant();
+        $restaurant->name = 'Le Bosphore';
+
+        $menu = new Menu();
+        $menu->name = "Menu 12/03";
+        $menu->hasMenuItem = [];
+
+        $restaurant->hasMenu = [$menu];
+
+        $content = $twig->render('menuList.html.twig',
+            [
+                'restaurant' => $restaurant
+            ]);
+
+        return new Response($content);
+    }
+
+
+    /**
+     * @Route("{restaurantName}/{menuName}", name="menu")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function menuPage(Environment $twig) {
+
+        $restaurant = new Restaurant();
+        $restaurant->name = 'Le Bosphore';
+
+        $menu = new Menu();
+        $menu->name = "Menu 12/03";
+        $menu->hasMenuItem = [];
+
+        $menuSection = new MenuSection();
+        $menuSection->name = 'Coucou';
+
+        $menuSection2 = new MenuSection();
+        $menuSection2->name = 'Coucou 2';
+        $menuSection2->hasMenuSection = [];
+
+
+        $menuSection2_1 = new MenuSection();
+        $menuSection2_1->name = 'Coucou 2-1';
+        $menuSection2_1->hasMenuItem = [];
+
+        $menuItem = new MenuItem();
+        $menuItem->name = "Kebab";
+        $menuItem->description = "De la viande Hallal";
+
+        $menuItem2 = new MenuItem();
+        $menuItem2->name = "KPop";
+        $menuItem2->description = "Viande Coreene";
+
+        $menuSection2->hasMenuItem[] = $menuItem2;
+
+        $menuSection2_1->hasMenuItem[] = $menuItem;
+
+        $menuSection2->hasMenuSection[] = $menuSection2_1;
+
+
+        $restaurant->hasMenu = [$menu];
         $menu->hasMenuSection[] = $menuSection;
         $menu->hasMenuSection[] = $menuSection2;
         $menu->hasMenuSection[] = new MenuSection();
